cffc257fa8d47d50d9187728a027c4dac6cb8f09
User_MZD <oossiioo@gmail.com>
2024-10-18 22:14:30 +0000

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -4,43 +4,29 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My App'),
-        ),
-        body: Center(
-          child: Text('Hello, World!'),
-        ),
-      ),
+      home: MyHomePage(title: 'My App'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({Key? key, required this.title});
+
+  MyHomePage({required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
         title: Text(title),
       ),
       body: Center(
         child: Text(
-          'Bist du bereit?',
+          'Hello, World!',
         ),
       ),
     );
